dae4624a9e97d10721ad90faab3a90e8da2fe78b
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-10 15:07:42 -0500

    Support version --output flag

--- pkg/azure/version.go
+++ pkg/azure/version.go
@@ -1,11 +1,19 @@
 package azure
 
 import (
-	"fmt"
-
 	"github.com/deislabs/porter-azure/pkg"
+	"github.com/deislabs/porter/pkg/mixin"
+	"github.com/deislabs/porter/pkg/porter/version"
 )
 
-func (m *Mixin) PrintVersion() {
-	fmt.Fprintf(m.Out, "Azure mixin %s (%s)\n", pkg.Version, pkg.Commit)
+func (m *Mixin) PrintVersion(opts version.Options) error {
+	metadata := mixin.Metadata{
+		Name: "azure",
+		VersionInfo: mixin.VersionInfo{
+			Version: pkg.Version,
+			Commit:  pkg.Commit,
+			Author:  "DeisLabs",
+		},
+	}
+	return version.PrintVersion(m.Context, opts, metadata)
 }
