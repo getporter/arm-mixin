f1edd8730a9d94975e481bc2968789f321b3e3da
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-10-08 13:43:44 -0600

    update after bad rebase

--- cmd/arm/version.go
+++ cmd/arm/version.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/deislabs/porter-arm/pkg/arm"
+	"github.com/deislabs/porter/pkg/porter/version"
 	"github.com/spf13/cobra"
 )
 
 func buildVersionCommand(m *arm.Mixin) *cobra.Command {
-	return &cobra.Command{
+
+	opts := version.Options{}
+
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print the mixin version",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
